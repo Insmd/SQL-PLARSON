1de0ee50c852c61f0f981a6a8283a5e561e40157
Insmd <sergo629@mail.ru>
2024-07-23 20:33:52 +0300

    Add files via upload

---
+++ SqlQuery_1.sql
@@ -0,0 +1,8 @@
+﻿-- SQL Запрос для получения месяцев и количества дней в месяце для 2024 года
+SELECT 
+    DATENAME(MONTH, DATEADD(MONTH, number, '2024-01-01')) AS Month, -- Получаем имя месяца
+    DAY(EOMONTH(DATEADD(MONTH, number, '2024-01-01'))) AS Days -- Получаем количество дней в месяце
+FROM master..spt_values -- Используем системную таблицу для генерации чисел
+WHERE type = 'P'
+AND number BETWEEN 0 AND 11 -- Ограничиваем диапазон чисел от 0 до 11
+ORDER BY number -- Сортируем по номеру месяца
